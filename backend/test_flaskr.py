b592ee09b5374d4e2497d6db2d293a1a152a321e
Roberto Zegers <rzegers@uc.cl>
2021-06-22 11:12:29 +0200

    Add test for for successful operation of the GET '/categories' endpoint

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -7,15 +7,16 @@ from flaskr import create_app
 from models import setup_db, Question, Category
 
 
-class TriviaTestCase(unittest.TestCase):
-    """This class represents the trivia test case"""
+class TriviaAppTestCase(unittest.TestCase):
+    """This class represents the Trivia App test case"""
 
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.user_name = "roberto"
+        self.database_path = "postgresql://{}@{}/{}".format(self.user_name,'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -26,14 +27,18 @@ class TriviaTestCase(unittest.TestCase):
             self.db.create_all()
     
     def tearDown(self):
-        """Executed after reach test"""
+        """Executed after each test"""
         pass
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+    def test_categories(self):
+        """Test Categories"""
+        res = self.client().get('/api/v1.0/categories')
+        data = json.loads(res.data)
 
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+        self.assertTrue(len(data['categories']))
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
